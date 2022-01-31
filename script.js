9c97bd23721cffeb0db7628296eb3e844ee6cd78
Tsubarah <Tsubarah@hotmail.se>
2022-01-31 10:41:35 +0100

    full commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const addTodo = todo => {
+  
+  const html = `
+  <li class="listgroup-item d-flex justify-content-between align-items-center p-2">
+  <span>${todo}</span>
+  <i class="fas fa-trash delete"></i>
+  </li>
+  `
+  list.innerHTML += html;
+}
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'))
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'))
+};
+
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+
+  filterTodos(term);
+})
+
+addForm.addEventListener('submit', e => {
+  
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  
+  if (todo.length) {
+    addTodo(todo);
+    addForm.reset();
+  }
+})
+
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+})
